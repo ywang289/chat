4f12f8bd2cfa57044ab9d021d16135e65a432015
ywang289 <56518323+ywang289@users.noreply.github.com>
2023-07-26 01:28:31 -0400

    new new

--- DFLT  WEB code/graph.py
+++ DFLT  WEB code/graph.py
@@ -1,22 +1,62 @@
-import matplotlib.pyplot as plt
+from bokeh.plotting import figure, show
+from bokeh.models import HoverTool, CustomJS, ColumnDataSource
+from PIL import Image
+import base64
 
-# 示例数据
-diameters = [10, 20, 30, 40, 50]  # 直径数据
-percentages = [15, 30, 45, 60, 75]  # 百分比数据
 
-# 创建图表
-plt.plot(diameters, percentages, marker='o', linestyle='-')
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return "data:image/png;base64," + base64.b64encode(image_file.read()).decode('utf-8')
 
-# 添加标题和轴标签
-plt.title('Diameter vs. Percentage')
-plt.xlabel('Diameter')
-plt.ylabel('Percentage')
+def generate_plot(radii, percentages, images):
+    x = [i for i in range(1, len(radii) + 1)]
+    y = radii
 
-# 设置x轴刻度
-plt.xticks(diameters)
+    source = ColumnDataSource(data=dict(
+        x=x,
+        y=y,
+        images=images,
+        percentages=percentages
+    ))
 
-# 显示网格线
-plt.grid(True)
+    p = figure(title="hirox result", plot_width=600, plot_height=400, background_fill_color='lightgrey')
+    p.circle('x', 'y', size=10, color="green", source=source)
 
-# 显示图表
-plt.show()
+    hover = HoverTool(tooltips="""
+        <div>
+            <div>
+                <img
+                    src="@images" height="200" alt="@percentages" width="200"
+                    style="float: left; margin: 0px 15px 15px 0px;"
+                ></img>
+            </div>
+            <div>
+                <span style="font-size: 17px; font-weight: bold;">@percentages</span>
+            </div>
+        </div>
+    """)
+
+    p.add_tools(hover)
+
+    tap_js = CustomJS(args=dict(source=source), code="""
+        var images = source.data['images'];
+        var index = source.selected.indices[0];
+        var dataURL = images[index];
+        var link = document.createElement('a');
+        link.href = dataURL;
+        link.download = 'image.png';
+        link.click();
+    """)
+
+    p.js_on_event('tap', tap_js)
+
+    show(p)
+
+radii = [3, 5, 8]
+percentages = [39.4, 28, 38.8]
+images = [
+    image_to_base64('DFLT  WEB code/templates/image4.png'),
+    image_to_base64('DFLT  WEB code/templates/image5.png'),
+    image_to_base64('DFLT  WEB code/templates/image6.png')
+]
+
